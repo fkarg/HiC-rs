513e8582f1f7c5e72df697c4a58c771933a19e1e
fkarg <f.karg10@gmail.com>
2019-03-08 16:22:00 +0100

    sync

---
+++ embed.py
@@ -0,0 +1,52 @@
+import ctypes
+
+# lib.process()
+
+
+
+class FFITuple(ctypes.Structure):
+    _fields_ = [("a", ctypes.c_uint32),
+                ("b", ctypes.c_uint32)]
+
+class FFIArray(ctypes.Structure):
+    _fields_ = [("data", ctypes.c_void_p),
+                ("len", ctypes.c_size_t)]
+
+    # Allow implicit conversions from a sequence of 32-bit unsigned
+    # integers.
+    @classmethod
+    def from_param(cls, seq):
+        return cls(seq)
+
+    # Wrap sequence of values. You can specify another type besides a
+    # 32-bit unsigned integer.
+    def __init__(self, seq, data_type = ctypes.c_uint32):
+        array_type = data_type * len(seq)
+        raw_seq = array_type(*seq)
+        self.data = ctypes.cast(raw_seq, ctypes.c_void_p)
+        self.len = len(seq)
+
+# A conversion function that cleans up the result value to make it
+# nicer to consume.
+def void_array_to_tuple_list(array, _func, _args):
+    tuple_array = ctypes.cast(array.data, ctypes.POINTER(FFITuple))
+    return [tuple_array[i] for i in range(0, array.len)]
+
+
+
+# lib = ctypes.cdll.LoadLibrary("./target/release/libhicrs.so")
+lib = ctypes.cdll.LoadLibrary("./target/debug/libhicrs.so")
+
+print("done!")
+
+
+
+# lib.listtest.argtypes = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
+lib.listtest.argtypes = FFIArray,
+lib.listtest.restype = FFIArray
+lib.listtest.errcheck = void_array_to_tuple_list
+
+list_to_sum = [1,2,3,4]
+c_array = (ctypes.c_int32 * len(list_to_sum))(*list_to_sum)
+l = lib.listtest(c_array, len(list_to_sum))
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,93 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate libc;
+
+use libc::{size_t,int32_t};
+use std::slice;
+use std::mem;
+
+use std::thread;
+
+#[no_mangle]
+fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in (0..5_000_000) {
+                x += 1
+            }
+        x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+        h.join().map_err(|_| "Could not join a thread!").unwrap());
     }
+    println!("done!");
 }
 
 
-use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
-
-
-#[pyfunction]
-/// Formats the sum of two numbers as string
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[no_mangle]
+fn double(x: i32) -> i32 {
+    x + x
 }
 
-/// This module is a python module implemented in Rust.
-#[pymodule]
-fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
-
-    Ok(())
+#[no_mangle]
+// fn listtest(data: *const i32, length: usize) -> Array {
+fn listtest(array: Array) -> Array {
+    // let nums = unsafe { slice::from_raw_parts(data, length as usize) };
+    let nums = unsafe { array.as_u32_slice() };
+    let data: Vec<u32> = nums.iter().map(|a| a * a).collect();
+	println!("Rust worked fine.");
+	Array::from_vec(data)
 }
+
+
+#[repr(C)]
+pub struct Tuple {
+    a: libc::uint32_t,
+    b: libc::uint32_t,
+}
+
+#[repr(C)]
+pub struct Array {
+    data: *const libc::c_void,
+    len: libc::size_t,
+}
+
+impl Array {
+    unsafe fn as_u32_slice(&self) -> &[u32] {
+		assert!(!self.data.is_null());
+		slice::from_raw_parts(self.data as *const u32, self.len as usize)
+	}
+
+    fn from_vec<T>(mut vec: Vec<T>) -> Array {
+		// Important to make length and capacity match
+		// A better solution is to track both length and capacity
+    	vec.shrink_to_fit();
+
+    	let array = Array { data: vec.as_ptr() as *const
+    		libc::c_void, len: vec.len() as libc::size_t };
+
+    	// Whee! Leak the memory, and now the raw pointer (and eventually C) is the owner.
+    	mem::forget(vec);
+		array
+	}
+}
+
+
+#[no_mangle]
+pub extern fn convert_vec(lon: Array, lat: Array) -> Array {
+    let lon = unsafe { lon.as_u32_slice() };
+    let lat = unsafe { lat.as_u32_slice() };
+
+	let vec =
+        lat.iter().zip(lon.iter())
+		   .map(|(&lat, &lon)| Tuple { a: lat, b: lon })
+           .collect();
+
+	Array::from_vec(vec)
+}
+
+
+
 
