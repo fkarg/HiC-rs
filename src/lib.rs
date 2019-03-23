ecbe7f953e888ffceb25ffb69b4e98b2143204c5
fkarg <f.karg10@gmail.com>
2019-03-24 00:36:53 +0100

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,33 @@
 extern crate libc;
 
-use libc::{size_t,int32_t};
-use std::slice;
+use libc::{int32_t, size_t};
 use std::mem;
+use std::slice;
 
 use std::thread;
 
 #[no_mangle]
 fn process() {
-    let handles: Vec<_> = (0..10).map(|_| {
-        thread::spawn(|| {
-            let mut x = 0;
-            for _ in (0..5_000_000) {
-                x += 1
-            }
-        x
+    let handles: Vec<_> = (0..10)
+        .map(|_| {
+            thread::spawn(|| {
+                let mut x = 0;
+                for _ in (0..5_000_000) {
+                    x += 1
+                }
+                x
+            })
         })
-    }).collect();
+        .collect();
 
     for h in handles {
-        println!("Thread finished with count={}",
-        h.join().map_err(|_| "Could not join a thread!").unwrap());
+        println!(
+            "Thread finished with count={}",
+            h.join().map_err(|_| "Could not join a thread!").unwrap()
+        );
     }
     println!("done!");
 }
-
 
 #[no_mangle]
 fn double(x: i32) -> i32 {
@@ -39,7 +42,6 @@ fn listtest(array: Array) -> Array {
     let data: Vec<u32> = nums.iter().map(|a| a * a).collect();
     Array::from_vec(data)
 }
-
 
 #[repr(C)]
 pub struct Tuple {
@@ -64,8 +66,10 @@ impl Array {
         // A better solution is to track both length and capacity
         vec.shrink_to_fit();
 
-        let array = Array { data: vec.as_ptr() as *const
-            libc::c_void, len: vec.len() as libc::size_t };
+        let array = Array {
+            data: vec.as_ptr() as *const libc::c_void,
+            len: vec.len() as libc::size_t,
+        };
 
         // Whee! Leak the memory, and now the raw pointer (and eventually C) is the owner.
         mem::forget(vec);
@@ -73,20 +77,16 @@ impl Array {
     }
 }
 
-
 #[no_mangle]
-pub extern fn convert_vec(lon: Array, lat: Array) -> Array {
+pub extern "C" fn convert_vec(lon: Array, lat: Array) -> Array {
     let lon = unsafe { lon.as_u32_slice() };
     let lat = unsafe { lat.as_u32_slice() };
 
-    let vec =
-        lat.iter().zip(lon.iter())
-           .map(|(&lat, &lon)| Tuple { a: lat, b: lon })
-           .collect();
+    let vec = lat
+        .iter()
+        .zip(lon.iter())
+        .map(|(&lat, &lon)| Tuple { a: lat, b: lon })
+        .collect();
 
     Array::from_vec(vec)
 }
-
-
-
-
