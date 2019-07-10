fc2d481b303a3bba0f59c2cd43232fd40e7f981b
fkarg <f.karg10@gmail.com>
2019-07-10 17:39:00 +0000

    Cleaned up Code

--- smb/embed.py
+++ smb/embed.py
@@ -6,14 +6,10 @@ from scipy.sparse import csr_matrix
 
 EXT = sys.platform == "darwin" and ".dylib" or ".so"
 
-# lib.process()
 
-
-class FFITuple(ctypes.Structure):
-    _fields_ = [("a", ctypes.c_uint32), ("b", ctypes.c_uint32)]
-
-    def __repr__(self):
-        return "({}, {})".format(self.a, self.b)
+# constructors for used FFI types
+# - List with 32bit int (unsigned)
+# - List with 64bit float (double)
 
 
 class FFIArray(ctypes.Structure):
@@ -36,30 +32,29 @@ class FFIArray(ctypes.Structure):
     def __len__(self):
         return self.len
 
+
 class FFIFloatArray(FFIArray):
     # Wrap sequence of values. You can specify another type besides a
-    # 32-bit unsigned integer.
+    # 64-bit float.
     def __init__(self, seq, data_type=ctypes.c_double):
         array_type = data_type * len(seq)
         raw_seq = array_type(*seq)
         self.data = ctypes.cast(raw_seq, ctypes.c_void_p)
         self.len = len(seq)
 
+
 # A conversion function that cleans up the result value to make it
 # nicer to consume.
-def void_array_to_tuple_list(array, _func, _args):
-    tuple_array = ctypes.cast(array.data, ctypes.POINTER(FFITuple))
-    return [tuple_array[i] for i in range(0, array.len)]
-
-
-def void_array_to_list(array, _func=None, _args=None):
+def void_array_to_double_list(array, _func=None, _args=None):
     l = array.len
-    a = ctypes.cast(array.data, ctypes.POINTER(ctypes.c_int))
+    a = ctypes.cast(array.data, ctypes.POINTER(ctypes.c_double))
     return [a[i] for i in range(l)]
 
 
+# necessary for relative finding of the actualy dynamic library
 my_path = os.path.abspath(os.path.dirname(__file__))
 
+# here should it be:
 path = os.path.join(my_path, "target/%s/libhicrs" + EXT)
 
 
@@ -67,54 +62,99 @@ try:
     lib = ctypes.cdll.LoadLibrary(path % "release")
     print("using release rs")
 except Exception as e:
-    lib = ctypes.cdll.LoadLibrary(path % "debug")
-    print("using debug rs")
+    try:
+        lib = ctypes.cdll.LoadLibrary(path % "debug")
+        print("using debug rs")
+    except:
+        lib = None
+        pass
 
 
-# lib.listtest.argtypes = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
+assert lib, "Could not load Rust library. Is it compiled?"
+
+
+
+# now, since the lib is loaded, let us add our one function correctly.
+
+# fn csrtest(indptr: Array, indices: Array, data: Array, threads: i32) -> Array {
+lib.wrapper_iterative_correct.argtypes = FFIArray, FFIArray, FFIFloatArray, ctypes.c_uint32
+lib.wrapper_iterative_correct.restype = FFIFloatArray # warning: test !!
+lib.wrapper_iterative_correct.errcheck = void_array_to_double_list
+
+
+def iterative_correct(matrix: csr_matrix, iternum = 50) -> list:
+    return lib.wrapper_iterative_correct(matrix.indptr, matrix.indices, matrix.data, iternum)
+
+
+
+
+
+
+
+
+
+# Further code, unrelated to smb directly but maybe relevant for understanding
+# the # interaction better:
+
+
+class FFITuple(ctypes.Structure):
+    _fields_ = [("a", ctypes.c_uint32), ("b", ctypes.c_uint32)]
+
+    def __repr__(self):
+        return "({}, {})".format(self.a, self.b)
+
+
+def void_array_to_tuple_list(array, _func, _args):
+    tuple_array = ctypes.cast(array.data, ctypes.POINTER(FFITuple))
+    return [tuple_array[i] for i in range(0, array.len)]
+
+
+def void_array_to_int_list(array, _func=None, _args=None):
+    l = array.len
+    a = ctypes.cast(array.data, ctypes.POINTER(ctypes.c_int))
+    return [a[i] for i in range(l)]
+
+
+
+
+
 lib.listtest.argtypes = (FFIArray,)
+# = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
 lib.listtest.restype = FFIArray
-lib.listtest.errcheck = void_array_to_list
-# lib.listtest.argtypes = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
-# lib.listtest.restype = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
-# lib.listtest.errcheck = void_array_to_tuple_list
+# = (ctypes.POINTER(ctypes.c_int32), ctypes.c_size_t)
+lib.listtest.errcheck = void_array_to_int_list
 listtest = lib.listtest
 
-list_to_sum = [1, 2, 3, 4]
+# list_to_sum = [1, 2, 3, 4]
 
 
 def cast_c(l):
+    # small helper function. was useful at some point.
     return (ctypes.c_int32 * len(l))(*l)
 
 
-# l = lib.listtest(cast_c(list_to_sum), len(list_to_sum))
-l = lib.listtest(list_to_sum)
+# l = lib.listtest(list_to_sum)
 
 
-row = np.array([0, 0, 1, 2, 2, 2])
-col = np.array([0, 2, 2, 0, 1, 2])
-data = np.array([1, 2, 3, 4, 5, 6])
+# row = np.array([0, 0, 1, 2, 2, 2])
+# col = np.array([0, 2, 2, 0, 1, 2])
+# data = np.array([1, 2, 3, 4, 5, 6])
+#
+#
+# matrix = csr_matrix((data, (row, col)), shape=(3, 3))
+# matrix2 = csr_matrix(
+#     ([1, 1, 2, 1, 3, 1, 4], ([0, 0, 1, 1, 2, 2, 3], [0, 1, 1, 2, 2, 3, 3])),
+#     shape=(4, 4),
+# )
 
-
-matrix = csr_matrix((data, (row, col)), shape=(3, 3))
-matrix2 = csr_matrix((
-    [ 1, 1, 2, 1, 3, 1, 4],
-    ([0, 0, 1, 1, 2, 2, 3]
-    ,[0, 1, 1, 2, 2, 3, 3])),
-    shape=(4,4))
-
-assert list(matrix.indptr) == list(np.array([0, 2, 3, 6]))
-assert list(matrix.indices) == list(np.array([0, 2, 2, 0, 1, 2]))
-
-
-lib.csrtest.argtypes = FFIArray, FFIArray, FFIFloatArray
-lib.csrtest.restype = FFIArray
-lib.csrtest.errcheck = void_array_to_list
-
-def iterative_correct(matrix: csr_matrix) -> list:
-    return lib.csrtest(matrix.indptr, matrix.indices, matrix.data)
-
-# e = iterative_correct(matrix)
+# assert list(matrix.indptr) == list(np.array([0, 2, 3, 6]))
+# assert list(matrix.indices) == list(np.array([0, 2, 2, 0, 1, 2]))
 
 
 
+# e = iterative_correct(matrix, 1)
+# print(e)
+
+
+# testing if calling multiple processes from rust works:
+# lib.process()
--- smb/src/lib.rs
+++ smb/src/lib.rs
@@ -1,37 +1,21 @@
-// #![feature(type_ascription)]
-#![allow(dead_code)]
-#![allow(unused_attributes)]
-
-
-// extern crate libc;
-// extern crate num;
-// extern crate rayon;
-// extern crate backtrace;
-
-use rayon::prelude::*;
-use std::fs;
-
+use std::process::Command;
 
 use num::traits::{zero, Num};
 
-// use libc::{int32_t, size_t};
 use std::mem;
 use std::slice;
 
 use std::thread;
 
-
-static THREAD_NUM: usize = 20;
-
-// use backtrace::Backtrace;
-
+/// Testing the spawning of processes. This operation is too fast to be a good
+/// example for parallelization.
 #[no_mangle]
 fn process() {
     let handles: Vec<_> = (0..10)
-        .map(|_| {
-            thread::spawn(|| {
-                let mut x = 0;
-                for _ in 0..5_000_000 {
+        .map(|i| {
+            thread::spawn(move || {
+                let mut x = i;
+                for _ in 0..500_000_000 {
                     x += 1
                 }
                 x
@@ -48,13 +32,14 @@ fn process() {
     println!("done!");
 }
 
+/// Also only test function.
 #[no_mangle]
 fn double(x: i32) -> i32 {
     x + x
 }
 
+/// For testing the passing of lists back and forth.
 #[no_mangle]
-// fn listtest(data: *const i32, length: usize) -> Array {
 fn listtest(array: Array) -> Array {
     // let nums = unsafe { slice::from_raw_parts(data, length as usize) };
     // println!("Got to the Rust side");
@@ -63,12 +48,14 @@ fn listtest(array: Array) -> Array {
     Array::from_vec(data)
 }
 
+/// For passing Tuples.
 #[repr(C)]
 pub struct Tuple {
     a: u32,
     b: u32,
 }
 
+/// For passing Arrays of yet unspecified type.
 #[derive(Debug)]
 #[repr(C)]
 pub struct Array {
@@ -82,6 +69,7 @@ impl Array {
         slice::from_raw_parts(self.data as *const u32, self.len as usize)
     }
 
+    #[allow(dead_code)]
     unsafe fn as_f32_slice(&self) -> &[f32] {
         assert!(!self.data.is_null());
         slice::from_raw_parts(self.data as *const f32, self.len as usize)
@@ -92,6 +80,7 @@ impl Array {
         slice::from_raw_parts(self.data as *const f64, self.len as usize)
     }
 
+    #[allow(dead_code)]
     unsafe fn as_usize_slice(&self) -> &[usize] {
         assert!(!self.data.is_null());
         slice::from_raw_parts(self.data as *const usize, self.len as usize)
@@ -113,6 +102,7 @@ impl Array {
     }
 }
 
+/// Another test function.
 #[no_mangle]
 pub extern "C" fn convert_vec(lon: Array, lat: Array) -> Array {
     let lon = unsafe { lon.as_u32_slice() };
@@ -127,7 +117,6 @@ pub extern "C" fn convert_vec(lon: Array, lat: Array) -> Array {
     Array::from_vec(vec)
 }
 
-
 fn invert_if_not_zero(num: f64) -> f64 {
     if num == 0. {
         num
@@ -136,6 +125,7 @@ fn invert_if_not_zero(num: f64) -> f64 {
     }
 }
 
+/// Implementation of Compressed Sparse Row Matrix. Barely has any features.
 #[repr(C)]
 #[derive(Debug)]
 pub struct CSRMatrix<T> {
@@ -164,23 +154,10 @@ impl<T: Num + Clone> CSRMatrix<T> {
         let (ind_begin, ind_end) = (self.indptr[coln], self.indptr[coln + 1]);
         let indices = &self.indices[ind_begin..ind_end];
 
-        // for index in 0..indices.len() {
-            // let row_index = indices[index];
         for (index, row_index) in indices.iter().enumerate() {
             let data_index = self.indptr[coln] + index;
             self.data[data_index] = f(&row_index, &self.data[data_index]);
         }
-    }
-
-    fn map_col_dbg<F>(&self, coln: usize, f: F)
-    where
-        F: Fn(&usize, &T),
-        // giving it the element and the index (within that col)
-    {
-        let (ind_begin, ind_end) = (self.indptr[coln], self.indptr[coln + 1]);
-        let indices = &self.indices[ind_begin..ind_end];
-        let slice = &self.data[ind_begin..ind_end];
-        let _inter: Vec<_> = slice.iter().zip(indices).map(|(d, i)| f(i, d)).collect();
     }
 
     fn get_col(&self, coln: usize, zero: T) -> Vec<T> {
@@ -196,6 +173,7 @@ impl<T: Num + Clone> CSRMatrix<T> {
         col
     }
 
+    #[allow(dead_code)]
     fn to_vec(&self) -> Vec<Vec<T>> {
         let mut full = Vec::new();
 
@@ -208,22 +186,9 @@ impl<T: Num + Clone> CSRMatrix<T> {
     }
 }
 
-#[derive(Debug)]
-pub struct COOMatrix<T> {
-    row: Vec<usize>,
-    col: Vec<usize>,
-    data: Vec<T>,
-}
-
-impl<T: Num + Clone> COOMatrix<T> {
-    fn new(row: Vec<usize>, col: Vec<usize>, data: Vec<T>) -> Self {
-        COOMatrix { row, col, data }
-    }
-}
-
+/// The actual function called from Python.
 #[no_mangle]
-fn csrtest(indptr: Array, indices: Array, data: Array) -> Array {
-    rayon::ThreadPoolBuilder::new().num_threads(THREAD_NUM).build_global().unwrap();
+fn wrapper_iterative_correct(indptr: Array, indices: Array, data: Array, numiter: i32) -> Array {
     let matrix = CSRMatrix::new(
         // indptr:
         unsafe { indptr.as_u32_slice() }
@@ -242,28 +207,25 @@ fn csrtest(indptr: Array, indices: Array, data: Array) -> Array {
             .collect(),
     );
 
-    // println!("CSRMatrix From Rust:\n{:?}", matrix);
-    // println!("Full CSRMatrix From Rust:\n{:?}", matrix.to_vec());
-
-    iterative_correction(matrix)
+    iterative_correction(matrix, numiter)
 }
 
-fn iterative_correction(mut matrix: CSRMatrix<f64>) -> Array {
+/// Here is the iterative correction actually happening.
+fn iterative_correction(mut matrix: CSRMatrix<f64>, numiter: i32) -> Array {
     println!("Starting iterative correction");
+
+    let tolerance = 1e-5;
+
     let mut biases = Vec::new();
+
     biases.resize(matrix.length, 1.0);
 
-    // let sum: f64 = matrix.data.par_iter().sum();
-    if matrix.data.par_iter().sum::<f64>().is_nan() {
-        // TODO: log.warn
-        println!(
+    if matrix.data.iter().sum::<f64>().is_nan() {
+        eprintln!(
             "[iterative correction] the matrix contains nans, they will be replaced by zeros."
         );
-        matrix.data = matrix.data.par_iter().map(|&v| 0_f64.max(v)).collect();
+        matrix.data = matrix.data.iter().map(|&v| 0_f64.max(v)).collect();
     }
-
-    let m = 500;
-    let tolerance = 1e-5;
 
     // Description of algorithm from paper:
     //
@@ -272,212 +234,109 @@ fn iterative_correction(mut matrix: CSRMatrix<f64>) -> Array {
     // We then divide W_ij by ^B_i*^B/j for all (i, j) and update the total vector of biases by
     // multiplying by the additional biases. Iterations are repeatet until the variance of the
     // additional biases becomes negligible; at this point W_ij has converged to T_ij.
-    //
-    // TODO: implement.
 
-    // def iterativeCorrection(matrix, v=None, M=50, tolerance=1e-5, verbose=False):
+    let mut biases = Vec::new();
 
-    // DONE
-    // total_bias = np.ones(matrix.shape[0], 'float64')
+    biases.resize(matrix.length, 1.0);
 
-    // DONE
-    // if np.isnan(matrix.sum()):
-    //     log.warn("[iterative correction] the matrix contains nans, they will be replaced by zeros.")
-    //     matrix.data[np.isnan(matrix.data)] = 0
-
-    // TODO: check for symmetry:
-    //
-    // if np.abs(matrix - matrix.T).mean() / (1. * np.abs(matrix.mean())) > 1e-10:
-    //     raise ValueError("Please provide symmetric matrix!")
-
-    // TODO ?
-    // W = matrix.tocoo()
-
-    // DONE
-    // for iternum in range(M):
-    //     iternum += 1
-    //     s = np.array(W.sum(axis=1)).flatten()
-    //     mask = (s == 0)
-    //     s = s / np.mean(s[~mask])
-
-    //      DONE
-    //     total_bias *= s
-    //     deviation = np.abs(s - 1).max()
-    //     s = 1.0 / s
-
-    //      TODO
-    //     # The following code  is an optimization of this
-    //     # for i in range(N):
-    //     #     for j in range(N):
-    //     #         W[i,j] = W[i,j] / (s[i] * s[j])
-    //
-    //     W.data *= np.take(s, W.row)
-    //     W.data *= np.take(s, W.col)
-
-    //     TODO
-    //     if np.any(W.data > 1e100):
-    //         log.error("*Error* matrix correction is producing extremely large values. "
-    //                   "This is often caused by bins of low counts. Use a more stringent "
-    //                   "filtering of bins.")
-    //         exit(1)
-
-    //     if deviation < tolerance:
-    //         break
-
-    // TODO
-    // # scale the total bias such that the sum is 1.0
-    // corr = total_bias[total_bias != 0].mean()
-    // total_bias /= corr
-    // W.data = W.data * corr * corr
-    // if np.any(W.data > 1e10):
-    //     log.error("*Error* matrix correction produced extremely large values. "
-    //               "This is often caused by bins of low counts. Use a more stringent "
-    //               "filtering of bins.")
-    //     exit(1)
-
-    // let mut iternum = 0;
     let mut s: Vec<f64> = Vec::with_capacity(matrix.length);
 
-    // println!("{:?}", matrix);
-
-    // DONE
-    // for iternum in range(M):
-    //     s = np.array(W.sum(axis=1)).flatten()
-    //     mask = (s == 0)
-    //     s = s / np.mean(s[~mask])
-    for _i in 0..m {
-    // for _i in 0..m {
+    for _i in 0..numiter {
         println!("[iteration]: {}", _i);
+        let mut s_sum: f64 = 0.0;
+
         for c in 0..matrix.length {
-            s.push(matrix.get_col(c, 0_f64).par_iter().sum());
-            // if c < 5 {
-                // matrix.map_col_dbg(c, |&i, v| println!("i: {}, v: {}, s[c]: {}", i, v, s[c]));
-                // println!("{:?}, sum: {}", matrix.get_col(c, 0_f64), s[c]);
-            // }
+            let colsum = matrix.get_col(c, 0_f64).iter().sum();
+            s_sum += colsum;
+            s.push(colsum);
         }
-        // DEBUG:
-        // s.iter().enumerate().filter(|(_i, &v)| v.is_infinite()).for_each(|(i, _)| println!("Found Inf at: {}", i));
-        let s_sum: f64 = s.iter().sum();
-        let mean: f64 = ((s.len() as f64) / s_sum) as f64; // inverse of: S_i / mean(S_i)
-        dbg!(mean);
-        s = s.par_iter().map(|&v: &f64| v * mean).collect(); // would have been v / mean, but the mean is the inverse already.
-        let _test: Vec<&f64> = dbg!(s.iter().take(10).collect());
 
-        //  DONE
-        // total_bias *= s
-        // deviation = np.abs(s - 1).max()
-        // s = 1.0 / s
-        biases = biases.par_iter().zip(s.clone()).map(|(a, b)| a * b).collect();
-        // biases = biases.iter().zip(s.clone()).map(|(a, b)| a * b).collect();
-        // let deviation: f64 = s.iter().fold(0_f64, |a, b| a.max((b - 1.).abs()));
-        // let deviation: f64 = s.par_iter().reduce(|| 0_f64, |a, b| a.max((b - 1.).abs()));
-        // let deviation: f64 = s.par_iter().fold_with(0_f64, |a, b| a.max((b - 1.).abs()));
-        // let deviation: f64 = s.par_iter()
-        let deviation: f64 = s.par_iter()
-            .cloned()
-            .map(|a| (a - 1_f64).abs())
-            .reduce(|| 0_f64, |a, b| a.max(b));
-            // .reduce(|| 0_f64, |a, b|
-            //         {
-            //         // println!("a: {} b: {} abs(b-1): {} max(a,b): {}", a, b, (b-1_f64).abs(), a.max((b - 1_f64).abs()));
-            //         a.max((b - 1_f64).abs())});
-            // .fold(0_f64, |a, b| a.max((b - 1.).abs()));
-        println!("deviation: {}", deviation);
-        dbg!(deviation);
+        let mean: f64 = ((matrix.length as f64) / s_sum) as f64; // inverse of: S_i / mean(S_i)
 
-        s = s.par_iter().map(|&v| invert_if_not_zero(v)).collect();
+        // would have been v / mean, but the mean is the inverse already.
+        s = s.iter().map(|&v: &f64| v * mean).collect();
 
-        // DONE
-        // # The following code  is an optimization of this
-        // # for i in range(N):
-        // #     for j in range(N):
-        // #         W[i,j] = W[i,j] / (s[i] * s[j])
-        //
-        // W.data *= np.take(s, W.row)
-        // W.data *= np.take(s, W.col)
+        biases = biases.iter().zip(s.clone()).map(|(a, b)| a * b).collect();
+
+        let deviation: f64 = s
+            .iter()
+            // for parallel:
+            // .iter_map()
+            // .map(|a| (a - 1_f64).abs())
+            // .reduce(|| 0_f64, |a, b| a.max(b));
+            .fold(0_f64, |a, b| a.max((b - 1.).abs()));
+
+
+        s = s.iter().map(|&v| invert_if_not_zero(v)).collect();
+
 
         // Iterate through all cols, and through the data in each col.
         // 'Generate' the index, from this multiply the s-value accordingly.
         // Also, for each col, have the other s-value (s[j]) cached for multiplication.
 
-        // for coln in 0..matrix.length {
+        // this would be way faster when parallel
         for (coln, c) in s.iter().enumerate().take(matrix.length) {
-            // let c = s[coln];
-            // if coln < 3 {
-            //     println!("Before updating values, c: {}", c);
-            //     matrix.map_col_dbg(coln, |&i, &v| println!("v: {}, s[i]: {}", v, s[i]));
-            // }
             matrix.map_col(coln, |&i, v| v * c * s[i]);
-            // if coln < 3 {
-            //     println!("After updating the values, coln {}", coln);
-            //     matrix.map_col_dbg(coln, |&i, &v| println!("v: {}, i: {}", v, i));
-            // }
         }
-        // DONE
-        // if np.any(W.data > 1e100):
-        //     log.error("*Error* matrix correction is producing extremely large values. "
-        //               "This is often caused by bins of low counts. Use a more stringent "
-        //               "filtering of bins.")
-        //     exit(1)
-        if matrix.data.iter().any(|&v| v > 1e100) {
-            println!("{:?}", matrix);
-            panic!("Error: matrix correction is producing extremely large values.")
-        }
+        s.clear();
 
-        // DONE
-        // if deviation < tolerance:
-        //     break
+        // Sufficient if checked in the end.
+        // if matrix.data.iter().any(|&v| v > 1e100) {
+        //     // println!("{:?}", matrix);
+        //     panic!("Error: matrix correction is producing extremely large values.")
+        // }
+
+
         if deviation < tolerance {
             println!("Deviation smaller tolerance! {} < {}", deviation, tolerance);
             break;
         }
-        s.clear();
-    }
-    drop(s);
 
-    // DONE
-    // # scale the total bias such that the sum is 1.0
-    // corr = total_bias[total_bias != 0].mean()
-    // total_bias /= corr
-    // W.data = W.data * corr * corr
-    // if np.any(W.data > 1e10):
-    //     log.error("*Error* matrix correction produced extremely large values. "
-    //               "This is often caused by bins of low counts. Use a more stringent "
-    //               "filtering of bins.")
-    //     exit(1)
+        // Printing information about current memory usage at the fifth
+        // iteration. Here everything should have stabilized.
+        if _i == 5 {
+            let memusage = Command::new("free")
+                .arg("-g")
+                .output()
+                .expect("failed to execute process");
+            let var = String::from_utf8(memusage.stdout).unwrap();
+            let out: Vec<&str> = var.split_whitespace().collect();
+            let memfree = out[8];
+            let memused = out[9];
+            println!("Memory used: {}", memfree);
+            println!("Memory free: {}", memused);
+        }
+    }
+    drop(s); // not needed any further, was only used for temporary biases
+
+    if matrix.data.iter().filter(|&v| *v > 1e10).any(|_| true) {
+        panic!("Error: matrix correction produced extremely large values");
+    }
+
+    // scale the total bias such that the sum is 1.0
 
     let (s, c) = biases
         .iter()
         .filter(|&v| *v > 0.0)
         .fold((0_f64, 0_f64), |(s, c), i| (s + i, c + 1.));
     let corr = s / c;
-    biases = biases.par_iter().map(|&v| v / corr ).collect();
+    let biases = biases.iter().map(|&v| v / corr).collect();
 
-    // println!("printing matrix data:");
-    for i in matrix.data.clone() {
-        if i > 1e10 {
-            panic!("Error: matrix correction produced extremely large values");
-        }
-        // println!("{}", i);
-    }
-    fs::write("tmpMatrix", format!("{:?}", matrix)).expect("Unable to write file");
 
     Array::from_vec(biases)
 }
 
-
-
 #[cfg(test)]
 pub mod tests {
-    use super::{CSRMatrix, iterative_correction};
+    use super::{iterative_correction, CSRMatrix};
 
     #[test]
     pub fn test_iterative_correct() {
         // let indptr  = vec![0, 1, 2, 3, 5, 6, 7, 8, 10, 11, 12];
         // let indices = vec![5 , 4 , 0 , 9 , 3 , 7 , 8 , 1 , 2 , 6, 3 , 4];
-        let indptr  = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let indices = vec![5 , 4 , 0 , 9 , 3 , 7 , 8 , 1 , 2 , 6];
-        let data    = vec![1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.];
+        let indptr = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let indices = vec![5, 4, 0, 9, 3, 7, 8, 1, 2, 6];
+        let data = vec![1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.];
         let matrix = CSRMatrix::new(indptr, indices, data);
         println!("{:?}", matrix);
         let e = iterative_correction(matrix);
@@ -486,15 +345,42 @@ pub mod tests {
 
     #[test]
     pub fn test_iterative_correct_2() {
-        let indptr = vec![0, 1, 1, 1, 1, 2, 3, 3, 3, 5, 6, 7, 7, 9, 9, 10, 11, 11, 13, 13, 16, 16, 17, 17, 17, 17, 19, 20, 23, 24, 25];
-        let indices = vec![5, 4, 0, 19, 25, 9, 12, 10, 14, 12, 19, 17, 25, 8, 15, 21, 19, 8, 17, 27, 26, 28, 29, 27, 27];
-        let data = vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
-        // let data' = vec![0.6944444444444445, 0.6944444444444445, 0.6944444444444445, 0.34722222222222227, inf, 0.6944444444444445, 0.6944444444444445, 0.34722222222222227, inf, 0.6944444444444445, 0.6944444444444445, 0.34722222222222227, inf, 0.2314814814814815, inf, inf, 0.6944444444444445, 0.34722222222222227, inf, 0.6944444444444445, 0.2314814814814815, inf, inf, 0.6944444444444445, 0.6944444444444445];
+        let indptr = vec![
+            0, 1, 1, 1, 1, 2, 3, 3, 3, 5, 6, 7, 7, 9, 9, 10, 11, 11, 13, 13, 16, 16, 17, 17, 17,
+            17, 19, 20, 23, 24, 25,
+        ];
+        let indices = vec![
+            5, 4, 0, 19, 25, 9, 12, 10, 14, 12, 19, 17, 25, 8, 15, 21, 19, 8, 17, 27, 26, 28, 29,
+            27, 27,
+        ];
+        let data = vec![
+            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        ];
         let matrix = CSRMatrix::new(indptr, indices, data);
         println!("{:?}", matrix);
         let e = iterative_correction(matrix);
         println!("{:?}", e);
     }
+
+    #[test]
+    pub fn test_to_vec() {
+        let indptr = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let indices = vec![5, 4, 0, 9, 3, 7, 8, 1, 2, 6];
+        let data = vec![1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.];
+        let matrix = CSRMatrix::new(indptr, indices, data);
+        let vec = vec![
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+            vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        ];
+        assert!(vec == matrix.to_vec());
+    }
 }
-
-
